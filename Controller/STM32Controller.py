c363f359b2285126890659dccbc2a1a30a8bcfa8
Jden <spect13317@gmail.com>
2025-02-05 20:53:24 +0800

    chore: rename rpi task 1 file

--- Controller/STM32Controller.py
+++ Controller/STM32Controller.py
@@ -1,6 +1,4 @@
 import serial
-# from settings import SERIAL_PORT, BAUD_RATE
-
 from BaseController import BaseController
 
 
@@ -12,12 +10,11 @@ class STM32Controller(BaseController):
         self.serial_link = None
 
     def connect(self): 
-        # SERIAL_PORT = '/dev/ttyS0' # TEMPORARY CHANGE
+        # SERIAL_PORT = '/dev/ttyS0' # WHEN DISCONNECTED
         SERIAL_PORT = '/dev/ttyUSB0'
         BAUD_RATE = 115200
         self.serial_link = serial.Serial(SERIAL_PORT, BAUD_RATE)
         self.logger.info(f"Connected to STM32 on {SERIAL_PORT}")
-
 
     def disconnect(self):
         self.serial_link.close()
@@ -25,10 +22,12 @@ class STM32Controller(BaseController):
         self.logger.info(f"Disconnected from STM32 on {SERIAL_PORT}")
 
     def send(self, msg: str) -> None:
+        # Send message to STM32
         self.serial_link.write(f"{msg}".encode('utf-8'))
         self.logger.info(f"Sent to STM32: {msg}")
 
     def receive(self) -> None:
+        # Read 4 bytes from serial link
         msg = self.serial_link.read(4).decode('utf-8')
         self.logger.info(f"Received from STM32: {msg}")
         return msg
